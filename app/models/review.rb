69292236309fed4ede5157f3e0db1c8779e5e325
nicola bergamaschi <nicola.bergamaschi@gmail.com>
2025-06-14 10:00:53 +0100

    model generation, db migration, db seeding

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, :rating, presence: true
+  validates :rating, comparison: { less_than_or_equal_to: 5}
+  validates :rating, numericality: { only_integer: true}
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,40 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+restaurant_count = 10
+
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+def random_category
+  CATEGORIES.sample
+end
+
+def random_rating
+  rand(1..5)
+end
+
+# Clean DB before seeding
+Review.destroy_all
+Restaurant.destroy_all
+
+restaurants = []
+restaurant_count.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: random_category
+  )
+  restaurants << restaurant
+end
+
+(restaurant_count * 2).times do
+  Review.create!(
+    rating: random_rating,
+    content: Faker::Restaurant.review,
+    restaurant: restaurants.sample
+  )
+end
