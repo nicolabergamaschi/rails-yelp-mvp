bfa672496ee93f5383a4089315cd500fa60d89ad
nicola bergamaschi <nicola.bergamaschi@gmail.com>
2025-06-14 12:12:38 +0100

    new, index, create for restaurant model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +8,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "restaurants", to: "restaurants#index"
+
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+
+  get "restaurants/:id", to: "restaurants#show"
+
+  post "restaurants", to: "restaurants#create"
+
 end
