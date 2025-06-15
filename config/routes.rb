62c4ed1fefd3486023ea3924425cc43b46a347c2
nicola bergamaschi <nicola.bergamaschi@gmail.com>
2025-06-15 10:13:27 +0100

    rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,12 +13,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get "restaurants", to: "restaurants#index"
-
-  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
-
-  get "restaurants/:id", to: "restaurants#show"
-
-  post "restaurants", to: "restaurants#create"
+ # get "restaurants", to: "restaurants#index"
+ # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+ # get "restaurants/:id", to: "restaurants#show", as: :show_restaurant
+ # post "restaurants", to: "restaurants#create"
+ # get 'restaurants/:id/reviews/new', to: "reviews#new", as: :new_review
 
 end
